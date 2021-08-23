c84c79532fcc077ccbdbebb6e5a1e9343805e483
Vish <vishvajitravalji@gmail.com>
2021-08-23 18:04:29 -0400

    portfolio deployed

---
+++ assets/scripts/app.js
@@ -0,0 +1,52 @@
+const Portfolio = function() {
+	
+
+
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+				"am Front-End Web Developer.", "am perfect & productive." , "love everything about code."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
